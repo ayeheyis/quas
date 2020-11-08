6d8d6760c31ad314a94c432d3876c63b67dfd30a
Ahadu <ahaduyeheyis@gmail.com>
2020-11-07 20:11:05 -0500

    postgres

--- quas/settings.py
+++ quas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'quas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quas',
+        'USER': 'quas',
+        'PASSWORD': 'quas',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
